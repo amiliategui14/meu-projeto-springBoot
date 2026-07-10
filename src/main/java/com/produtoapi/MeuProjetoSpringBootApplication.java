1c1efdacb330150cf71846e898f2f742fa8f2993
Rafaela Amiliategui <amiliateguirafaela@gmail.com>
2026-07-10 19:57:14 -0300

    Primeiro commit do projeto Spring Boot

---
+++ src/main/java/com/produtoapi/MeuProjetoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpringBootApplication.class, args);
+	}
+
+}
